bcc25ce229c0849982f6bce7bfae661b46bddc60
Steven James <pyro@linuxlabs.com>
2023-09-14 02:47:37 -0400

    Add advanced configuration to treat empty layer 0 errors as warnings.

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -355,11 +355,12 @@ GCodeGenerator::ObjectsLayerToPrint GCodeGenerator::collect_layers_to_print(cons
         // Check that there are extrusions on the very first layer. The case with empty
         // first layer may result in skirt/brim in the air and maybe other issues.
         if (layers_to_print.size() == 1u) {
-            if(!has_extrusions) {
-                if(!config.errors_are_warnings) {
+            if(!config.errors_are_warnings) {
+                if (!has_extrusions)
                     throw Slic3r::SlicingError(_u8L("There is an object with no extrusions in the first layer.") + "\n" +
                                            _u8L("Object name") + ": " + object.model_object()->name);
-                } else {
+            } else {
+                if (!has_extrusions) {
                     std::string float_warning;
 
                     float_warning += Slic3r::format(_u8L("First layer is empty. This may not be what you want")) +"\n";
