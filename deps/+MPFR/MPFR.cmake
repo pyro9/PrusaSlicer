519a778917d7c38a3b7408441bb25f49c9e90edc
Steven James <pyro@linuxlabs.com>
2023-11-11 16:53:18 -0500

    Run aclocal and automake on upstream MPFR so it can build on Bionic.

--- deps/+MPFR/MPFR.cmake
+++ deps/+MPFR/MPFR.cmake
@@ -35,6 +35,7 @@ else ()
         URL_HASH SHA256=b9df93635b20e4089c29623b19420c4ac848a1b29df1cfd59f26cab0d2666aa0
         DOWNLOAD_DIR ${${PROJECT_NAME}_DEP_DOWNLOAD_DIR}/MPFR
         BUILD_IN_SOURCE ON
+        PATCH_COMMAND aclocal && automake
         CONFIGURE_COMMAND env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --prefix=${${PROJECT_NAME}_DEP_INSTALL_PREFIX} --enable-shared=no --enable-static=yes --with-gmp=${${PROJECT_NAME}_DEP_INSTALL_PREFIX} ${_gmp_build_tgt}
         BUILD_COMMAND make -j
         INSTALL_COMMAND make install
